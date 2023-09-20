bcb654420b6337931d6938cd109f913c1bfb236f
sruti <srutikaparapu@gmail.com>
2023-09-20 20:51:18 +0530

    netflix

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBsG0u6bb6hfbf3oWVjZAZhqdVr0O9ida4",
+    authDomain: "netflixp-4041a.firebaseapp.com",
+    projectId: "netflixp-4041a",
+    storageBucket: "netflixp-4041a.appspot.com",
+    messagingSenderId: "408406474059",
+    appId: "1:408406474059:web:7449d3adc2897893fdbcce",
+    measurementId: "G-3P7GGJFK9M"
+  };
